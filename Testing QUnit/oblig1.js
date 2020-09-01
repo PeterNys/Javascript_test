79bdd5a5601ea80390cbc7ed3b374865941fb58e
PeterNys <peter@getacademy.no>
2020-09-01 14:59:06 +0200

    Update oblig1.js

--- Testing QUnit/oblig1.js
+++ Testing QUnit/oblig1.js
@@ -1,4 +1,5 @@
-function mainFunction(txt){
+function 
+mainFunction(txt){
 lengthFunction(txt);
 punctumFunction(txt);
 }
@@ -16,11 +17,19 @@ function punctumFunction(txt){
     }
     return false;
 }
-function atLeastFourFunction(txt){
-    let year = txt.substr(6,9);
-    console.log((year >= '0000',year <= '9999' ))
-    if (year >= '0000' && year <= '9999'){
-        return true;        
-    }
-    return false;
+function yearFunction(txt){
+    let year = txt.substr(6,4);
+    if (year.parseInt=NaN){return false};
+    return true;
+}
+function monthFunction(txt){
+    let month = txt.substr(3,2);
+    if (month.parseInt=NaN){return false};
+    if (month>'12'||month<'01'){console.log("month is incorrect");return false;}
+    return true;
+}
+function dayFunction(txt){
+    let day = txt.substr(0,2);
+    if (day.parseInt=NaN){return false};
+    if (day>)
 }
