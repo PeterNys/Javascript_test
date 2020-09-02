13076e7c6a6efaade4d5c68c263f861ee60216e2
PeterNys <peter@getacademy.no>
2020-09-02 15:35:42 +0200

    oblig 1 finish

--- Testing QUnit/lib.js
+++ Testing QUnit/lib.js
@@ -1,10 +1,17 @@
 
-
-function fixText(txt){
-    var trimmedText =txt.trim().toLowerCase();
-    let firstLetter = trimmedText.charAt(0).toUpperCase();
-    let lastPart = trimmedText.substring(1);
-    trimmedText = firstLetter+lastPart;
-    return trimmedText;
-     
-}
+function dayFunction(txt){
+    let year='1993';
+    let month='02';
+    day = txt.substr(0,2);
+    if (day.parseInt=NaN){return false};
+    if (day<1){return false};
+    if (day=='31'&&(month=='1'||month=='3'||month=='5'||month=='7'||month=='8'||month=='10'||month=='12')){
+        return true;
+    }
+    if (day<='30'&&month!='02'){return true;}
+    if (day=='29' && isLeapYear(year)){
+        return true;
+    }
+    if (day<='28'){return true;}
+    return false;
+    }
